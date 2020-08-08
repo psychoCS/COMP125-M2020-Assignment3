580505894d9b3f024daa5b60ca76546cf12cd5fc
psychoCS <thiagoluizb@hotmail.com>
2020-08-07 23:31:52 -0400

    Cleanned the code and proper commented

--- Scripts/app.js
+++ Scripts/app.js
@@ -42,22 +42,24 @@ File Description: Simple css file with all the codes for my first portfolio page
     function Paragraphs() {
         console.info("Paragraphs Loading...");
 
-        // step 1 - creates the XHR object
+        // Creates the Object
         let PAR = new XMLHttpRequest();
 
-        // step 2 - configures the message
+        // That will open the Json file
         PAR.open("GET", "Scripts/paragraphs.json");
 
-        // step 3 - Executes the request
+        // And Send the information... 
         PAR.send();
 
-        // step 4 - register the readystate event 
+        // ...Here, so the JS can read it, slice it and inject it in the proper places 
         PAR.addEventListener("readystatechange", function () {
             if ((PAR.readyState === 4) && (PAR.status === 200)) {
 
                 let paragraph = JSON.parse(PAR.responseText);
                 var paragraphArray = paragraph.Paragraphs;
                 let variable = document.getElementsByClassName("text")[0];
+
+                // Loop to se witch page you are.
                 if (variable = document.getElementsByClassName("text")[0]) {
                     document.getElementsByClassName("text")[0].innerHTML = paragraphArray[0].Introduction;
                 } else if (variable = document.getElementsByClassName("text1")[0]) {
@@ -91,118 +93,6 @@ File Description: Simple css file with all the codes for my first portfolio page
             }
         });
     }
-    /*
-        function IntroText() {
-            console.log("successfully added the introduction paragraph");
-
-            // Let's get the element on the page
-            let text = document.getElementsByClassName("text")[0];
-
-            if (text) {
-                // Then lets create a new div element
-
-                let newDiv = document.createElement("div");
-
-                // Configure
-                newDiv.innerHTML =
-                    `
-                    <p> It's really tricky to talk about yourself </p>
-                    <p> But I will try to keep as simples as possible</p>              
-                    <p> Brazilian, Born in 82 </p>
-                    <p> Together with my soul mate since 2011 </p>                
-                    <p> Living in Canada with her since 2019 </p>
-                    <p> Marvel Maniac (Comics, Games, Movies) </p>
-                    <p> Love games that get you new experiences </p>
-                    <p> My main goal is to turn all this chaos in codes</p>
-                    <p> Do let's go through the portal "Alice" </p>
-
-                    `;
-
-                // step 4 attach the new element
-                text.appendChild(newDiv);
-
-                return true;
-            }
-            return false;
-        }
-        
-
-    function projectsTextA() {
-        console.log("successfully added project A paragraph");
-
-        let intro = document.getElementsByClassName("text1")[0];
-
-        if (intro) {
-
-            let newDiv1 = document.createElement("div");
-
-            newDiv1.innerHTML =
-                `
-                <p> CINECOM </p>
-                <p> Here is where I push myself </p>
-                <p> and learn new tricks about </p>     
-                <p> video editing </p>     
-     
-                `;
-
-            intro.appendChild(newDiv1);
-
-            return true;
-        }
-        return false;
-    }
-
-    function projectsTextB() {
-        console.log("successfully added project B paragraph");
-
-        let text2 = document.getElementsByClassName("text2")[0];
-
-        if (text2) {
-
-            let newDiv2 = document.createElement("div");
-
-            newDiv2.innerHTML =
-                `
-                <p> CODICTS </p>
-                <p> I just love to see now new ideas </p>
-                <p> on coding using java or css </p>     
-                <p> And for me Codicts is one of the best </p>     
-     
-                `;
-
-            text2.appendChild(newDiv2);
-
-            return true;
-        }
-        return false;
-    }
-
-    function projectsTextC() {
-        console.log("successfully added project C paragraph");
-
-        let text3 = document.getElementsByClassName("text3")[0];
-
-        if (text3) {
-
-            let newDiv3 = document.createElement("div");
-
-            newDiv3.innerHTML =
-                `
-                <p> Two Minute Papers </p>
-                <p> I just love A.I. </p>
-                <p> That something that I really </p>     
-                <p> want to work with, and this channel </p>
-                <p> Show me crazy stuff </p>          
-     
-                `;
-
-            text3.appendChild(newDiv3);
-
-            return true;
-        }
-        return false;
-    }
-    */
 
     function validateForm() {
         let contactUsForm = document.forms[0];
@@ -255,16 +145,7 @@ File Description: Simple css file with all the codes for my first portfolio page
 
         Paragraphs()
 
-        /* IntroText();
-
-        // projectsTextA();
-
-        projectsTextB();
-
-        projectsTextC();*/
-
         validateForm();
-
 
     }
 
